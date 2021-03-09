d346f0b02272d43917cfb72bed24795ef4e6e212
Marvin Lüthe <marvin-luethe@gmx.de>
2021-03-09 18:59:43 +0100

    filteredimage endpoint added

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -17,7 +17,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -30,12 +29,39 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
+
+async function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
   
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+  // filteredimage Endpoint
+  app.get( "/filteredimage/", async ( req, res ) => {
+    // Validate image_url query
+    let { image_url } = req.query;
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`Image URL is required.`);
+    }
+    try {
+      // Call filterImageFromURL to filter the image
+      let filtered_image = await filterImageFromURL(image_url);
+
+      // Send the resulting file in the response
+      res.status(200).sendFile(filtered_image);
+
+      // Delete any files in the tmp folder
+      await delay(2000);
+      await deleteLocalFiles([filtered_image]);
+    } catch(err) {
+      // Send response that provided URL was not good
+      return res.status(400).send(`This is not a valid URL.`);
+    }
   } );
+
+  // Root
+  app.get("/", async(req, res) => {
+    return res.send("root")
+  });
   
 
   // Start the Server
